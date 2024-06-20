1774f81a2291a53afdf44c5905b949b93752101b
DESKTOP-MK268CN\Mel <lavienvert3@gmail.com>
2024-06-20 12:54:43 +0200

    minor fix

--- app/src/main/java/com/melaniadev/fitcare/ui/DataMocker.kt
+++ app/src/main/java/com/melaniadev/fitcare/ui/DataMocker.kt
@@ -2,21 +2,21 @@ package com.melaniadev.fitcare.ui
 
 fun mockList(): List<Customer> {
     val customersList = listOf(
-        Customer("Bernat", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Mel", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Marina", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Estela", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Nadine", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Ariel", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Lorena", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Lucia", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Nil", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Roc", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Nana", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Teo", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Jordi", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Clara", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null),
-        Customer("Carmen", "25/03/23", "07/05/24", "Marco Aurelio", null, null, null, null, null, null, null, null)
+        Customer("Bernat", "25/03/23", "07/05/24", "Marco Aurelio", Gender.MALE, null, null, null, null, null, null, null),
+        Customer("Mel", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null),
+        Customer("Marina", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null),
+        Customer("Estela", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null),
+        Customer("Nadine", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null),
+        Customer("Ariel", "25/03/23", "07/05/24", "Marco Aurelio", Gender.MALE, null, null, null, null, null, null, null),
+        Customer("Lorena", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null),
+        Customer("Lucia", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null),
+        Customer("Nil", "25/03/23", "07/05/24", "Marco Aurelio", Gender.MALE, null, null, null, null, null, null, null),
+        Customer("Roc", "25/03/23", "07/05/24", "Marco Aurelio", Gender.MALE, null, null, null, null, null, null, null),
+        Customer("Nana", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null),
+        Customer("Teo", "25/03/23", "07/05/24", "Marco Aurelio", Gender.MALE, null, null, null, null, null, null, null),
+        Customer("Jordi", "25/03/23", "07/05/24", "Marco Aurelio", Gender.MALE, null, null, null, null, null, null, null),
+        Customer("Clara", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null),
+        Customer("Carmen", "25/03/23", "07/05/24", "Marco Aurelio", Gender.FEMALE, null, null, null, null, null, null, null)
     )
     return customersList
 }
